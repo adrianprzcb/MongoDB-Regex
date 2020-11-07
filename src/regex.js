e0e1d8438144e3f3e896e56fe1d38b7273ea2b5a
aperezc <aperezc@iespuntadelverde.es>
2020-11-07 13:27:54 +0100

    first commit

---
+++ src/regex.js
@@ -0,0 +1,12 @@
+db.cars.insertMany([
+    { "_id" : 100, "pais" : "Alemania", "Coches Electricos" : 216264, "Puntos de Recarga" : 5074 },
+    { "_id" : 101, "pais" : "Reino Unido", "Coches Electricos" : 212651, "Puntos de Recarga" : 4008 },
+    { "_id" : 102, "pais" : "Noruega", "Coches Electricos" : 274207, "Puntos de Recarga" : 3426 },
+    { "_id" : 103, "pais" : "Francia", "Coches Electricos" : 180277, "Puntos de Recarga" : 1975 },
+    { "_id" : 104, "pais" : "España", "Coches Electricos" : 33211, "Puntos de Recarga" : 1242 },
+    { "_id" : 105, "pais" : "Holanda", "Coches Electricos" : 152510, "Puntos de Recarga" : 1069 },
+    { "_id" : 106, "pais" : "Suecia", "Coches Electricos" : 87031, "Puntos de Recarga" : 1023 },
+    { "_id" : 107, "pais" : "Suiza", "Coches Electricos" : 37596, "Puntos de Recarga" : 846 },
+    { "_id" : 108, "pais" : "Italia", "Coches Electricos" : 25363	, "Puntos de Recarga" : 833 },
+    { "_id" : 109, "pais" : "Austria", "Coches Electricos" : 30273, "Puntos de Recarga" : 575 },
+])
